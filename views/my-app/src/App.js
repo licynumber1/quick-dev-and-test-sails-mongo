4e471f89df48edc1974b569eddd41e77b76a1a9b
lee <476770392@qq.com>
2019-02-28 10:35:48 +0800

    清除部分测试代码

--- views/my-app/src/App.js
+++ views/my-app/src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import '../node_modules/antd/dist/antd.min.css'
 import Collection from './Collection.js'
-import { Example } from './component'
 
 //需要校验的集合：name代表名称，api代表接口访问地址、searchForm为查询搜索条件、form为编辑表单.
 //form和searchForm： type代表对应组件、parse代表接口字段名、dis为描述。src为部分支持async组件获取数据用、srcMap代表对应label字段名，modalParse代表输出api字段名
@@ -43,9 +42,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {
-          <Example />
-        }
         {
           groups.map(i=>{
             return <Collection data={i}/>
